3f5c572adcc93e949951c548f5b8aa08c2e869f8
hitesh1210 <hiteshkadu137@gmail.com>
2022-08-09 08:30:23 -0400

    error in submodule

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220809.193213';
+const cacheName = 'chirpy-20220809.175919';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
